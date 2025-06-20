bf755e143b3273b9bb164372d1c369d5c05273ea
leotyps <113692736+leotyps@users.noreply.github.com>
2025-06-20 21:09:35 +0700

    Update vercel.js

--- vercel.js
+++ vercel.js
@@ -3,12 +3,21 @@
   "builds": [
     {
       "src": "src/index.ts",
-      "use": "@vercel/node"
+      "use": "@vercel/node",
+      "config": { "zeroConfig": true }
     }
   ],
   "routes": [
     {
-      "src": "/(.*)",
+      "src": "/api/(.*)",
+      "dest": "src/index.ts"
+    },
+    {
+      "src": "/webhook/(.*)",
+      "dest": "src/index.ts"
+    },
+    {
+      "src": "/$",
       "dest": "src/index.ts"
     }
   ]
